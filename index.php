adb17bd6845df2d3c2f47471829636da731a2cbc
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-14 10:37:49 +0100

    inserita variabile contenente stringa

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+$stringa = 'Sopra la panca la capra campa,
+sotto la panca la capra crepa.
+
+Sopra la panca la capra campa,
+sotto la panca la capra crepa.
+
+Sopra la panca la capra campa,
+sotto la panca la capra crepa.';
 
 ?>
 
